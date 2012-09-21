0a9d3e36ad933b24d6d29297d66da849d228db9e
Jason Carter <jccarte@gmail.com>
2012-09-21 15:31:25 -0400

    finished chapter 9 mostly

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,46 +13,57 @@ require 'spec_helper'
 
 describe User do
 
-	before do
-		 @user = User.new(name: "Example User", email: "user@example.com",
-		  				  password: "foobar", password_confirmation: "foobar")
-	end
+  before do
+     @user = User.new(name: "Example User", email: "user@example.com",
+                                    password: "foobar", password_confirmation: "foobar")
+  end
 
-	subject { @user }
+  subject { @user }
 
-	it { should respond_to(:name) }
-	it { should respond_to(:email) }
-	it { should respond_to(:password_digest) }
-	it { should respond_to(:password) }
-	it { should respond_to(:password_confirmation) }
-	it { should respond_to(:remember_token) }
-	it { should respond_to(:authenticate) }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
-	it { should be_valid }
+  it { should be_valid }
+  it { should_not be_admin }
 
-	describe "when name is not present" do
-		before {@user.name = " " }
-		it { should_not be_valid }
-	end
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
 
-	describe "when name is too long" do
-		before {@user.name = "a" * 51 }
-		it { should_not be_valid }
-	end
+    it { should be_admin }
+  end
 
-	describe "when email is not present" do
-		before {@user.email = " " }
-		it { should_not be_valid }
-	end
+  describe "when name is not present" do
+    before {@user.name = " " }
+    it { should_not be_valid }
+  end
 
-	describe "when email format is invalid" do
+  describe "when name is too long" do
+    before {@user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before {@user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         @user.should_not be_valid
-      end      
+      end
     end
   end
 
@@ -62,7 +73,7 @@ describe User do
       addresses.each do |valid_address|
         @user.email = valid_address
         @user.should be_valid
-      end      
+      end
     end
   end
 
@@ -86,18 +97,18 @@ describe User do
   end
 
   describe "when password is not present" do
-  	before { @user.password = @user.password_confirmation = " " }
-  	it { should_not be_valid }
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
   end
 
   describe "when password doesn't match confirmation" do
-  	before { @user.password_confirmation = "mismatch" }
-  	it { should_not be_valid }
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
   end
 
   describe "when password confirmation is nil" do
-  	before { @user.password_confirmation = nil }
-  	it { should_not be_valid }
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
   end
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
